b6eb52dbe20baff667b61093bb8d5543ca6c1ea3
peethree <115643299+peethree@users.noreply.github.com>
2023-03-14 15:24:45 +0100

    Update chembaby.py

--- chembaby.py
+++ chembaby.py
@@ -1,3 +1,6 @@
+# copyright © 2023 P.B. "peethree" Schellekens
+# see bottom for extended copyright information 
+
 import sys
 import matplotlib.pyplot as plt
 
@@ -80,3 +83,8 @@ def plot_data(data_dict):
 if __name__ == "__main__":
     main()
 
+# Chembaby is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
